9be2e570dbecc97ba281859b1c7bf3884fcf5113
Zain Albastaki <zain.albastaki@gmail.com>
2021-05-21 10:49:04 +0300

    Add grades page

--- src/views/student/StudentGrades.vue
+++ src/views/student/StudentGrades.vue
@@ -6,8 +6,44 @@
                 <base-text type="h1">Grades</base-text>
                 <progress-section />
             </section>
-            <!-- TO DO -->
-            <base-text type="p">Coming soon!</base-text>
+            <base-section
+                v-for="(clas, index) in classes"
+                :key="`class-${index}`"
+                class="class"
+                :heading="clas.name"
+                :background="`${clas.color}44`"
+                :border="clas.color"
+            >
+                <base-text v-if="clas.assessments.length === 0" type="p">
+                    There are no assessments set for this class yet.
+                </base-text>
+                <template v-else>
+                    <div
+                        v-for="(assessment, index) in clas.assessments"
+                        :key="`assessment-${index}`"
+                        class="assessment"
+                        :style="{ borderColor: clas.color }"
+                    >
+                        <div
+                            class="type"
+                            :style="{
+                                background: `${clas.color}44`,
+                                borderColor: clas.color,
+                            }"
+                        >
+                            {{ assessment.type }}
+                        </div>
+                        <div class="name" :style="{ borderColor: clas.color }">
+                            {{ assessment.name }}
+                        </div>
+                        <div class="grade">
+                            {{ getGrade(assessment.id) }}/{{
+                                assessment.total_grade
+                            }}
+                        </div>
+                    </div>
+                </template>
+            </base-section>
         </div>
     </base-dashboard>
 </template>
@@ -24,7 +60,21 @@
         },
 
         computed: {
-            ...mapGetters(['profile']),
+            ...mapGetters(['classes', 'profile']),
+        },
+
+        methods: {
+            getGrade(id) {
+                const submissions = this.profile.submissions.filter(
+                    ({ assessmentId }) => assessmentId === id
+                );
+                const grades = submissions.map(({ grade }) => parseInt(grade));
+                grades.forEach((grade, i) => {
+                    if (isNaN(grade)) grades.splice(i, 1);
+                });
+                if (grades.length === 0) return;
+                return Math.max(...grades);
+            },
         },
     };
 </script>
@@ -38,6 +88,53 @@
 
             .h1 {
                 font-size: 50px;
+            }
+        }
+
+        .class {
+            margin-bottom: 20px;
+
+            /deep/ .content {
+                padding: 0;
+            }
+
+            .assessment {
+                display: flex;
+
+                &:not(:last-of-type) {
+                    border-bottom-width: 2px;
+                    border-bottom-style: solid;
+                }
+
+                .type {
+                    border-right-width: 2px;
+                    border-right-style: solid;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    min-width: 130px;
+                    text-transform: capitalize;
+                    font-weight: bold;
+                }
+
+                .name {
+                    flex: 1;
+                    display: flex;
+                    align-items: center;
+                    border-right-width: 2px;
+                    border-right-style: solid;
+                    padding: 10px;
+                }
+
+                .grade {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    min-width: 100px;
+                    font-weight: bold;
+                    font-size: 18px;
+                    padding: 10px 0;
+                }
             }
         }
     }
